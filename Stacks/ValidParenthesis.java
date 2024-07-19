f2992b08773bc9caea24c5dbedd15915f9515df0
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2024-07-19 23:22:40 +0530

    .

---
+++ Stacks/ValidParenthesis.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static boolean isValid(String s){
+
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch=='(' ||ch== '{' || ch== '['){
+                st.push(ch);
+            }else{
+                if (st.isEmpty()) return false;
+                char top = st.peek();
+                if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
