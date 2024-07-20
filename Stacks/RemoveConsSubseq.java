44ab05cccec0ed609d1ad685a4e79583b8be8303
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2024-07-20 23:18:04 +0530

    Create RemoveConsSubseq.java

---
+++ Stacks/RemoveConsSubseq.java
@@ -0,0 +1,24 @@
+package Stacks;
+import java.util.*;
+public class RemoveConsSubseq {
+
+    public void Remove(arr){
+        Stack<Integer> st = new Stack<>();
+
+    for (int i=0;i<n;i++){
+        if(st.size()==0) st.push(arr[i]);
+        
+    }
+
+    }
+
+    
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,3,5,6,6,6,7,8};
+        
+
+    }
+    
+}
